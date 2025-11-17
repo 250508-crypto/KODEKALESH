6402b244c7b26ab5dece3151411d8ac87ecbb4e2
250508-crypto <250508@kit.ac.in>
2025-11-17 23:09:34 +0530

    Add files via upload

---
+++ summerapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# admin.site.register(addcart)
+admin.site.register(product)
+admin.site.register(cart)
+admin.site.register(category)
+admin.site.register(orderItem)
+
+# Register your models here.
